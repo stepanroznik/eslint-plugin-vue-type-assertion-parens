0c6de9f6c914c186e3af23cf989a2403926ab3a4
Štěpán Rožník <stepan.roznik@cloverdx.com>
2025-06-23 09:07:34 +0200

    feat: add plugin index

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import typeAssertionParens from './rules/type-assertion-parens';
+
+const plugin = {
+  rules: {
+    'type-assertion-parens': typeAssertionParens,
+  },
+  configs: {
+    recommended: {
+      plugins: ['vue-type-assertion-parens'],
+      rules: {
+        'vue-type-assertion-parens/type-assertion-parens': 'error',
+      },
+    },
+  },
+};
+
+export = plugin;
